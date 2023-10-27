a4e4d39d5bc8bd81cc7864c1af2a2470713931d0
NimishRamteke-GEP <87696087+NimishRamteke-GEP@users.noreply.github.com>
2023-10-28 02:10:01 +0530

    Initial commit

---
+++ ShellScript/Logtash_KeyStore_Remove.sh
@@ -0,0 +1,7 @@
+sudo -E /usr/share/logstash/bin/logstash-keystore --path.settings /etc/logstash remove es_auth_token
+sudo -E /usr/share/logstash/bin/logstash-keystore --path.settings /etc/logstash remove es_pwd
+sudo -E /usr/share/logstash/bin/logstash-keystore --path.settings /etc/logstash remove es_url
+sudo -E /usr/share/logstash/bin/logstash-keystore --path.settings /etc/logstash remove es_usr
+sudo -E /usr/share/logstash/bin/logstash-keystore --path.settings /etc/logstash remove nr_license_key
+
+
